888e87e8103debf525e1e7c656357d0d9b6ad29c
Antony Faris <acp325@gmail.com>
2022-12-13 15:10:20 -0500

    Use Vercel (#32)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from "astro/config";
-// https://astro.build/config
 import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
